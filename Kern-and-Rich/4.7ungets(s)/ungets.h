8b4967b85871df2f9db654bf5cd3a7f2aa4ecc5d
eBenya <entotMail.com>
2020-01-22 11:08:12 +0300

    division of projects into branches: libr

--- Kern-and-Rich/4.7ungets(s)/ungets.h
+++
@@ -1,6 +0,0 @@
-void ungets(char s[]) {
-
-	int i = strlen(s);
-
-	while (i > 0) ungetch(s[--i]);
-}
--- cs50change/part4/int  factorial(int  n).cpp
+++
@@ -1,5 +0,0 @@
-int  factorial(int  n) {
-	
-	if (n < 1) return 0;
-	return n + factorial(n - 1);
-}
